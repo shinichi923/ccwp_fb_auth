319513e482443cbd10a938c03a67e842d8c2ef77
shinichi923 <shinichi923@gmail.com>
2014-01-22 10:14:36 +0800

    Simpel fb friend list

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,6 @@
 class UserController < ApplicationController
   def friend_list
+  	@graph = Koala::Facebook::API.new(current_user.oauth_token)
+  	@friends = @graph.get_connection("me", "friends")
   end
 end
